bfd45cbeefcc029afb9e79b4a5296c0e09d28a08
MahalaxmiSadula <sadulamahalaxmi9@gmail.com>
2024-01-16 18:40:18 -0800

    first commit

---
+++ src/main/java/com/code/fortunegenerator/FortuneGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.code.fortunegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/fortunegenerator/FortuneGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.fortunegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
